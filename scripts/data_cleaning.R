752aed7599890dd435df65eeffa1e4b5933d138f
IsabellaSegarra <irsegarra@outlook.com>
2026-04-21 22:29:33 -0700

    Update final csvs.

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -20,35 +20,21 @@ redlist <- read_csv(here("data", "raw", "points_data.csv"))
 
 # Protected areas ----
 
-pa <- st_read(here("data","raw", "WDPA_WDOECM_Apr2026_Public_SA_shp_0"))
+pa_0 <- st_read(here("data","raw", "WDPA_WDOECM_Apr2026_Public_SA_shp_0", "WDPA_WDOECM_Apr2026_Public_SA_shp-polygons.shp"))
 
-# Countries boundaries ----
-countries_sf <- st_read("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson")
+
+pa_1 <- st_read(here("data","raw","WDPA_WDOECM_Apr2026_Public_SA_shp_1", "WDPA_WDOECM_Apr2026_Public_SA_shp-polygons.shp"))
+
+pa_2 <- st_read(here("data","raw", "WDPA_WDOECM_Apr2026_Public_SA_shp_2", 
+                    "WDPA_WDOECM_Apr2026_Public_SA_shp-polygons.shp"))
+
+pa_raw <- bind_rows(pa_0, pa_1, pa_2)
+
 
 # GBIF vernacular/common names data ----
 vernacular <- read_tsv(here("data", "raw", "VernacularName.tsv"))
 
 #......Data cleaning......
-
-# GBIF vernacular names
-vernacular_en_sp <- vernacular %>%
-  clean_names() %>% 
-  filter(language %in% c("en", "es")) %>% 
-  pivot_wider(names_from = language, values_from = vernacular_name) %>% 
-  select(taxon_id, en, es)
-
-common_names_tbl <- vernacular %>%
-  clean_names() %>% 
-  filter(language %in% c("en", "es")) %>%
-  group_by(taxon_id, language) %>%
-  slice(1) %>%
-  ungroup() %>%
-  pivot_wider(id_cols = taxon_id, names_from = language, values_from = vernacular_name)
-
-species <- unique(species_tbl$scientific_name)
-
-common_names_tbl <- left_join(species_tbl, common_names_tbl, by = c("species_key = taxon_id"))
-
 
 # gbif ---- Produce tables 
 
@@ -58,23 +44,37 @@ species_tbl <- gbif %>%
   select(species_key, scientific_name, genus, family, order, class, phylum, kingdom, taxon_rank) %>%
   distinct(species_key, .keep_all = TRUE) %>% 
 # Remove author citation regex 
- mutate(scientific_name = sub(" [\\(A-Z].*$", "", scientific_name))
+ mutate(scientific_name = sub(" [\\(A-Z].*$", "", scientific_name)) %>% 
+  rename(species_id = species_key)
 
-# Clean up scientific name 
-# join common names back 
+# GBIF vernacular names
+
+# Common names table 
+common_names_tbl <- vernacular %>%
+  clean_names() %>%
+  filter(language %in% c("en", "es"),
+         taxon_id %in% species_tbl$species_id) %>%
+  group_by(taxon_id, language) %>%
+  slice(1) %>%
+  ungroup() %>%
+  pivot_wider(id_cols = taxon_id,
+              names_from = language,
+              values_from = vernacular_name) %>%
+  rename(species_id = taxon_id, common_name_en = en, common_name_es = es)
 
 # Occurrence table
 occurrences_tbl <- gbif %>%
-  clean_names() %>% 
-  select(species_key, country_code, decimal_latitude, decimal_longitude, event_date, year, month, day, occurrence_status, individual_count, coordinate_uncertainty_in_meters, institution_code) %>% 
+  clean_names()%>% 
   filter(
     !is.na(decimal_latitude),
     !is.na(decimal_longitude),
     coordinate_uncertainty_in_meters < 10000)  %>% # drop anything with >10km uncertainty
   st_as_sf(coords = c("decimal_longitude", "decimal_latitude"), crs = 4326) %>% 
-  mutate(event_date = as_datetime(event_date))
+  mutate(event_date = as_date(event_date), 
+         species_id = species_key) %>% 
+  select(species_key, country_code, event_date, year, month, day, occurrence_status, individual_count) 
   
-
+  
 # Country table 
 countries_tbl <- gbif %>%
   clean_names() %>%
@@ -92,42 +92,41 @@ countries_tbl <- gbif %>%
     country_code == "UY" ~ "Uruguay"
   ))
 
-# Aggregated occurrences table
-species_occurrence_tbl <- occurrences_tbl %>%
-  group_by(species_key, country_code, year) %>%
-  summarise(
-    n_occurrences = n(),
-    .groups = "drop"
-  )
-
 #..... Protected Areas ......
 
 # join with occurence, create binary indicator (within PA YES/NO --> protected_area ID)
 
-protected_areas_tbl <- pa %>% 
+protected_areas_tbl <- pa_raw %>% 
   clean_names() %>% 
   mutate(pa_id = site_id, 
          name_sp = name, 
-         desig_sp = desig
+         desig_es = desig, 
+         country_code = iso3,
          ) %>% 
-  select(pa_id, name_eng, name_sp, desig_eng, desig_sp, desig_type,iucn_cat, realm, status_yr, gov_type, geometry)
+  filter(country_code %in% c("BRA", "COL", "ARG", "CHL", "ECU", "PER", "VEN", "URY", "BOL")) %>% 
+  select(pa_id, country_code, name_eng, name_sp, desig_eng, desig_es, desig_type, realm, status_yr, gov_type, geometry) %>% mutate(country_code= case_when(
+    country_code == "COL" ~ "CO",
+    country_code == "BRA" ~ "BR",
+    country_code == "VEN" ~ "VE",
+    country_code == "ARG" ~ "AR",
+    country_code == "CHL" ~ "CL",
+    country_code == "PER" ~ "PE",
+    country_code == "BOL" ~ "BO",
+    country_code == "ECU" ~ "EC",
+    country_code == "URY" ~ "UY"
+  ))
 
-# Add countries to protected-areas table
 
-countries_sa <- countries_sf %>% 
-  filter(name %in% c("Colombia", "Brazil", "Venezuela", "Argentina", 
-                     "Chile", "Peru", "Bolivia", "Ecuador", "Uruguay"))
-
-protected_areas_tbl <- st_join(protected_areas_tbl,
-                               countries_sa %>% select(ISO3166.1.Alpha.2, geometry),
-                               join = st_intersects) %>%
-  rename(country_code = ISO3166.1.Alpha.2)
-
-# Species protection status table 
+# Protected status table
 
 # Transform to a projected CRS for accurate buffering in metres
 occurrences_proj <- st_transform(occurrences_tbl, 3857)
 pa_proj <- st_transform(protected_areas_tbl, 3857)
+
+pa_proj <- st_make_valid(pa_proj)
+
+occurrences_proj <- occurrences_proj %>%
+  st_cast("POINT")
 
 # Buffer protected areas by e.g. 10km
 pa_buffered <- st_buffer(pa_proj, dist = 10000)
