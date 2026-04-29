a57c02b2a1246ffef040dbc346cdb036b59d8c19
IsabellaSegarra <irsegarra@outlook.com>
2026-04-29 13:33:16 -0700

    Update protection status dataframe.

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -1,4 +1,4 @@
-# Load in packages
+ # Load in packages
 library(tidyverse)
 library(here)
 library(dplyr)
@@ -8,15 +8,17 @@ library(data.table)
 library(rredlist)
 library(janitor)
 library(iucnredlist)
+library(ARTofR)
 
-# ......Import data ......
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+##------------------------------- IMPORT DATA --------------------------------
+##                                                                            --
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 # GBIF occurrence data ----
 gbif <- fread(here("data", "raw", "gbif_sa.csv")) 
-
-# Redlist data ----
-
-redlist <- read_csv(here("data", "raw", "points_data.csv"))
 
 # Protected areas ----
 
@@ -26,7 +28,7 @@ pa_0 <- st_read(here("data","raw", "WDPA_WDOECM_Apr2026_Public_SA_shp_0", "WDPA_
 pa_1 <- st_read(here("data","raw","WDPA_WDOECM_Apr2026_Public_SA_shp_1", "WDPA_WDOECM_Apr2026_Public_SA_shp-polygons.shp"))
 
 pa_2 <- st_read(here("data","raw", "WDPA_WDOECM_Apr2026_Public_SA_shp_2", 
-                    "WDPA_WDOECM_Apr2026_Public_SA_shp-polygons.shp"))
+                     "WDPA_WDOECM_Apr2026_Public_SA_shp-polygons.shp"))
 
 pa_raw <- bind_rows(pa_0, pa_1, pa_2)
 
@@ -34,20 +36,24 @@ pa_raw <- bind_rows(pa_0, pa_1, pa_2)
 # GBIF vernacular/common names data ----
 vernacular <- read_tsv(here("data", "raw", "VernacularName.tsv"))
 
-#......Data cleaning......
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+##------------------------------- DATA CLEANING  -------------------------------
+##                                                                            --
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# gbif ---- Produce tables 
+# GBIF ---- 
 
 # Species table
 species_tbl <- gbif %>%
   clean_names() %>%
   select(species_key, scientific_name, genus, family, order, class, phylum, kingdom, taxon_rank) %>%
   distinct(species_key, .keep_all = TRUE) %>% 
-# Remove author citation regex 
- mutate(scientific_name = sub(" [\\(A-Z].*$", "", scientific_name)) %>% 
+  # Remove author citation regex 
+  mutate(scientific_name = sub(" [\\(A-Z].*$", "", scientific_name)) %>% 
   rename(species_id = species_key)
 
-# GBIF vernacular names
+# GBIF vernacular names ----
 
 # Common names table 
 common_names_tbl <- vernacular %>%
@@ -63,18 +69,26 @@ common_names_tbl <- vernacular %>%
   rename(species_id = taxon_id, common_name_en = en, common_name_es = es)
 
 # Occurrence table
-occurrences_tbl <- gbif %>%
+raw_occurrences_tbl <- gbif %>%
   clean_names()%>% 
   filter(
     !is.na(decimal_latitude),
     !is.na(decimal_longitude),
-    coordinate_uncertainty_in_meters < 10000)  %>% # drop anything with >10km uncertainty
+    coordinate_uncertainty_in_meters < 10000, # drop anything with >10km uncertainty
+    occurrence_status == "PRESENT")  %>% # Drop any occurrences that are absent
   st_as_sf(coords = c("decimal_longitude", "decimal_latitude"), crs = 4326) %>% 
-  mutate(event_date = as_date(event_date), 
-         species_id = species_key) %>% 
-  select(species_key, country_code, event_date, year, month, day, occurrence_status, individual_count) 
-  
-  
+  mutate(event_date = as_date(event_date)) %>% 
+  select(species_key, country_code, event_date, year, month, day, occurrence_status, individual_count) %>% 
+  rename(species_id = species_key)
+
+# Aggregated occurrences (LOOK INTO GEOMETRY COLUMN)
+
+occurrences_tbl <- raw_occurrences_tbl %>%
+  st_drop_geometry() %>%
+  group_by(species_id, country_code, year) %>%
+  summarize(occurrence = sum(individual_count, na.rm = TRUE), .groups = "drop")
+
+
 # Country table 
 countries_tbl <- gbif %>%
   clean_names() %>%
@@ -102,7 +116,7 @@ protected_areas_tbl <- pa_raw %>%
          name_sp = name, 
          desig_es = desig, 
          country_code = iso3,
-         ) %>% 
+  ) %>% 
   filter(country_code %in% c("BRA", "COL", "ARG", "CHL", "ECU", "PER", "VEN", "URY", "BOL")) %>% 
   select(pa_id, country_code, name_eng, name_sp, desig_eng, desig_es, desig_type, realm, status_yr, gov_type, geometry) %>% mutate(country_code= case_when(
     country_code == "COL" ~ "CO",
@@ -120,33 +134,33 @@ protected_areas_tbl <- pa_raw %>%
 # Protected status table
 
 # Transform to a projected CRS for accurate buffering in metres
-occurrences_proj <- st_transform(occurrences_tbl, 3857)
-pa_proj <- st_transform(protected_areas_tbl, 3857)
+occurrences_proj <- st_transform(raw_occurrences_tbl, 3857)
+pa_proj <- st_transform(protected_areas_tbl, 3857) %>% st_make_valid()
 
-pa_proj <- st_make_valid(pa_proj)
 
 occurrences_proj <- occurrences_proj %>%
   st_cast("POINT")
 
-# Buffer protected areas by e.g. 10km
-pa_buffered <- st_buffer(pa_proj, dist = 10000)
-
-# Now intersect
-occurrences_proj <- occurrences_proj %>%
-  mutate(in_protected_area = lengths(st_intersects(occurrences_proj, pa_buffered)) > 0)
-
-protection_sts_tbl <- st_join(occurrences_proj %>% select(species_key, geometry),
-                            pa_buffered %>% select(pa_id, geometry),
-                            join = st_intersects) %>%
+protection_sts_tbl <- st_join(
+  occurrences_proj %>% select(species_id, geometry),
+  pa_proj %>% select(pa_id, geometry),
+  join = st_intersects
+) %>%
   mutate(in_protected_area = !is.na(pa_id))
 
+protection_sts_tbl <- protection_sts_tbl %>% st_drop_geometry()
 
-# Export as csv into data_processed folder 
-
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+##------------------------------- EXPORT --------------------------------
+##                                                                            --
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 species_tbl <- write_csv(species_tbl, "data/processed/species.csv")
+occurrences_raw_tbl <- write_csv(raw_occurrences_tbl, "data/processed/occurrences_raw.csv")
 occurrences_tbl <- write_csv(occurrences_tbl, "data/processed/occurrences.csv")
 countries_tbl <- write_csv(countries_tbl, "data/processed/countries.csv")
 protected_areas_tbl <- write_csv(protected_areas_tbl, "data/processed/protected_areas.csv")
+protection_sts_tbl <- write_csv(protection_sts_tbl, "data/processed/protection_sts.csv")
 
